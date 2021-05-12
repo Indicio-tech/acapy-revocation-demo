2f8f6f7fe8f9701bbbbc5c7d88271291b907696e
Daniel Bluhm <dbluhm@pm.me>
2021-05-12 00:11:22 -0400

    feat: partial demo in place
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- Makefile
+++ Makefile
@@ -4,4 +4,10 @@ holder:
 	$(CONTAINER_RUNTIME) run --rm -it -p 3000:3000 -p 3001:3001 \
 		-v $(abspath ./configs):/home/indy/configs:z \
 		bcgovimages/aries-cloudagent:py36-1.16-0_0.6.0 \
-		start --arg-file ./configs/holder.yml
+		start --arg-file ./configs/holder.yml -e http://rocket:3000
+
+issuer:
+	$(CONTAINER_RUNTIME) run --rm -it -p 3002:3000 -p 3003:3001 \
+		-v $(abspath ./configs):/home/indy/configs:z \
+		bcgovimages/aries-cloudagent:py36-1.16-0_0.6.0 \
+		start --arg-file ./configs/issuer.yml -e http://rocket:3002
--- acapy_revocation_demo/__main__.py
+++ acapy_revocation_demo/__main__.py
@@ -1,1 +1,84 @@
 """Run the demo."""
+
+import json
+import os
+from acapy_client.models.schema_send_request import SchemaSendRequest
+import httpx
+
+from acapy_client import Client
+from acapy_client.api.connection import create_invitation, receive_invitation
+from acapy_client.api.wallet import create_did, set_public_did
+from acapy_client.api.schema import publish_schema
+from acapy_client.models import (
+    CreateInvitationRequest,
+    ReceiveInvitationRequest,
+)
+
+
+HOLDER_URL = os.environ.get("HOLDER", "http://localhost:3001")
+ISSUER_URL = os.environ.get("ISSUER", "http://localhost:3003")
+
+
+def describe(description: str, api):
+    def _describe(**kwargs):
+        print(description)
+        request = api._get_kwargs(**kwargs)
+        print("Request:", json.dumps(request, indent=2))
+        result = api.sync(**kwargs)
+        assert result
+        print("Response:", json.dumps(result.to_dict(), indent=2))
+        return result
+
+    return _describe
+
+
+def main():
+    """Run steps."""
+    holder = Client(base_url=HOLDER_URL)
+    issuer = Client(base_url=ISSUER_URL)
+
+    invite = describe("Create new invitation in holder", create_invitation)(
+        client=holder, json_body=CreateInvitationRequest(), auto_accept="true"
+    )
+
+    describe("Receive invitation in issuer", receive_invitation)(
+        client=issuer,
+        json_body=ReceiveInvitationRequest.from_dict(invite.invitation.to_dict()),
+    )
+
+    did_info = describe(
+        "Create new DID for publishing to ledger in issuer", create_did
+    )(client=issuer).result
+
+    print("Publishing DID through https://selfserve.indiciotech.io")
+    response = httpx.post(
+        url="https://selfserve.indiciotech.io/nym",
+        json={
+            "network": "testnet",
+            "did": did_info.did,
+            "verkey": did_info.verkey,
+        },
+    )
+    if response.is_error:
+        print("Failed to publish DID:", response.text)
+        return
+    print("DID Published")
+
+    result = describe("Set DID as public DID for issuer", set_public_did)(
+        client=issuer, did=did_info.did
+    ).result
+    print(result.posture)
+
+    result = describe("Publish schema to the ledger", publish_schema)(
+        client=issuer,
+        json_body=SchemaSendRequest(
+            attributes=["firstname", "age"],
+            schema_name="revocation_testing",
+            schema_version="0.1.0",
+        ),
+    )
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
