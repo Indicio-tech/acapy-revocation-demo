bba15e4df1b3a5c90b3fa8fe4cfc7515eb675124
Daniel Bluhm <dbluhm@pm.me>
2021-05-12 12:55:47 -0400

    feat: finish end-to-end demonstration
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- acapy_revocation_demo/__main__.py
+++ acapy_revocation_demo/__main__.py
@@ -2,19 +2,36 @@
 
 import json
 import os
-from acapy_client.models.schema_send_request import SchemaSendRequest
-import httpx
+import time
+from typing import cast
 
 from acapy_client import Client
 from acapy_client.api.connection import create_invitation, receive_invitation
-from acapy_client.api.wallet import create_did, set_public_did
-from acapy_client.api.schema import publish_schema
+from acapy_client.api.credential_definition import publish_cred_def
+from acapy_client.api.issue_credential_v_10 import issue_credential_automated
+from acapy_client.api.present_proof import send_proof_request, get_present_proof_records
 from acapy_client.api.ledger import accept_taa, fetch_taa
+from acapy_client.api.schema import publish_schema
+from acapy_client.api.revocation import revoke_credential, publish_revocations
+from acapy_client.api.wallet import create_did, set_public_did
 from acapy_client.models import (
     CreateInvitationRequest,
     ReceiveInvitationRequest,
     TAAAccept,
+    CredentialDefinitionSendRequest,
+    SchemaSendRequest,
+    V10CredentialProposalRequestMand,
+    CredAttrSpec,
+    CredentialPreview,
+    IndyProofRequest,
+    IndyProofRequestRequestedAttributes,
+    IndyProofRequestRequestedPredicates,
+    V10PresentationSendRequestRequest,
+    RevokeRequest,
+    V10CredentialExchange,
+    PublishRevocations,
 )
+import httpx
 
 
 HOLDER_URL = os.environ.get("HOLDER", "http://localhost:3001")
@@ -40,13 +57,15 @@ def main():
     issuer = Client(base_url=ISSUER_URL)
 
     # Establish Connection {{{
-    invite = describe("Create new invitation in holder", create_invitation)(
+    holder_conn_record = describe("Create new invitation in holder", create_invitation)(
         client=holder, json_body=CreateInvitationRequest(), auto_accept="true"
     )
 
-    describe("Receive invitation in issuer", receive_invitation)(
+    issuer_conn_record = describe("Receive invitation in issuer", receive_invitation)(
         client=issuer,
-        json_body=ReceiveInvitationRequest.from_dict(invite.invitation.to_dict()),
+        json_body=ReceiveInvitationRequest.from_dict(
+            holder_conn_record.invitation.to_dict()
+        ),
     )
     # }}}
 
@@ -85,7 +104,6 @@ def main():
     result = describe("Set DID as public DID for issuer", set_public_did)(
         client=issuer, did=did_info.did
     ).result
-    print(result.posture)
     # }}}
 
     # Prepare Credential ledger artifacts {{{
@@ -97,7 +115,94 @@ def main():
             schema_version="0.1.0",
         ),
     )
-    print(result)
+
+    issuer.timeout = 30
+    result = describe(
+        "Publish credential definition with revocation support", publish_cred_def
+    )(
+        client=issuer,
+        json_body=CredentialDefinitionSendRequest(
+            revocation_registry_size=10,
+            schema_id=result.schema_id,
+            support_revocation=True,
+        ),
+    )
+    issuer.timeout = 5
+    # }}}
+
+    # Issue Credential and request presentation {{{
+    issue_result = describe("Issue credential to holder", issue_credential_automated)(
+        client=issuer,
+        json_body=V10CredentialProposalRequestMand(
+            connection_id=issuer_conn_record.connection_id,
+            credential_proposal=CredentialPreview(
+                attributes=[
+                    CredAttrSpec(name="firstname", value="Bob"),
+                    CredAttrSpec(name="age", value="42"),
+                ]
+            ),
+            cred_def_id=result.credential_definition_id,
+        ),
+    )
+    issue_result = cast(V10CredentialExchange, issue_result)
+    time.sleep(1)
+    result = describe("Request proof from holder", send_proof_request)(
+        client=issuer,
+        json_body=V10PresentationSendRequestRequest(
+            connection_id=issuer_conn_record.connection_id,
+            proof_request=IndyProofRequest(
+                name="proof of name",
+                version="0.1.0",
+                requested_attributes=IndyProofRequestRequestedAttributes.from_dict(
+                    {
+                        "firstname": {
+                            "name": "firstname",
+                            "non_revoked": {"to": int(time.time())},
+                        }
+                    }
+                ),
+                requested_predicates=IndyProofRequestRequestedPredicates(),
+            ),
+        ),
+    )
+    time.sleep(1)
+    result = describe("List presentations", get_present_proof_records)(client=issuer)
+    # }}}
+
+    # Revoke credential and request presentation {{{
+    result = describe("Revoke credential", revoke_credential)(
+        client=issuer,
+        json_body=RevokeRequest(
+            cred_ex_id=issue_result.credential_exchange_id, publish=False
+        ),
+    )
+    result = describe("Publish revocations", publish_revocations)(
+        client=issuer, json_body=PublishRevocations()
+    )
+    time.sleep(10)
+    result = describe(
+        "Request proof from holder again after revoking", send_proof_request
+    )(
+        client=issuer,
+        json_body=V10PresentationSendRequestRequest(
+            connection_id=issuer_conn_record.connection_id,
+            proof_request=IndyProofRequest(
+                name="proof of name",
+                version="0.1.0",
+                requested_attributes=IndyProofRequestRequestedAttributes.from_dict(
+                    {
+                        "firstname": {
+                            "name": "firstname",
+                            "non_revoked": {"to": int(time.time())},
+                        }
+                    }
+                ),
+                requested_predicates=IndyProofRequestRequestedPredicates(),
+            ),
+        ),
+    )
+    time.sleep(1)
+    result = describe("List presentations", get_present_proof_records)(client=issuer)
     # }}}
 
 
